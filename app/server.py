a85635c60e603396884418e40ab38a19ea33e020
WarRusher <47946314+WarRusher@users.noreply.github.com>
2019-06-16 08:29:45 +0100

    Updated classes and export vars

--- app/server.py
+++ app/server.py
@@ -9,10 +9,19 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/file/d/1BwnOpJgoj3f-6IGOgtazQUk8QgnNFIpn/view?usp=sharing'
+export_file_name = 'cars99.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Convertible',
+ 'Coupe',
+ 'Hatchback',
+ 'Microcar',
+ 'Minivan',
+ 'Pickup Truck',
+ 'Sedan',
+ 'Sport Utlity Vehicle',
+ 'Sports Car',
+ 'Station Wagon']
 path = Path(__file__).parent
 
 app = Starlette()
